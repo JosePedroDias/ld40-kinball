8267ce92bf3b6852e4cc31fb63408ea7bb4ddc69
José Pedro Dias <jose.pedro.dias@gmail.com>
2017-12-02 19:38:51 +0000

    plunger, bumper

--- src/flipper.js
+++ src/flipper.js
@@ -29,15 +29,21 @@ function linearize(n, a, b) {
 
 const KC_Z = 90;
 const KC_M = 77;
+const KC_R = 82;
 const KC_SPACE = 32;
-const KCS = [KC_Z, KC_M, KC_SPACE];
+const KC_LEFT = 37;
+const KC_RIGHT = 39;
+const KC_UP = 38;
+const KC_DOWN = 40;
+const KCS = [KC_Z, KC_M, KC_R, KC_DOWN];
 
 const keyIsDown = {};
 
 function hookKeys() {
   document.addEventListener("keydown", ev => {
     const kc = ev.keyCode;
-    if (!inArr(kc, KCS)) {
+    console.log(kc);
+    if (ev.ctrlKey || ev.altKey || ev.shiftKey || !inArr(kc, KCS)) {
       return;
     }
     ev.preventDefault();
@@ -50,7 +56,7 @@ function hookKeys() {
 
   document.addEventListener("keyup", ev => {
     const kc = ev.keyCode;
-    if (!inArr(kc, KCS)) {
+    if (ev.ctrlKey || ev.altKey || ev.shiftKey || !inArr(kc, KCS)) {
       return;
     }
     ev.preventDefault();
@@ -70,6 +76,24 @@ function polarMove({ pos, r, angle }) {
   return [pos[0] + Math.cos(a) * r, pos[1] + Math.sin(a) * r];
 }
 
+function createArc({ pos, r, a0, a1, steps, dims, options }) {
+  let a = a0;
+  const da = (a1 - a0) / steps;
+
+  const parts = [];
+  for (let i = 0; i <= steps; ++i) {
+    const p = polarMove({ pos, r, angle: a });
+    const rect = M.Bodies.rectangle(p[0], p[1], dims[0], dims[1], {
+      angle: a * DEG2RAD
+    });
+    parts.push(rect);
+
+    a += da;
+  }
+
+  return M.Body.create({ parts, ...options });
+}
+
 function createFlipperShape({ pos, ra, rb, length, options }) {
   const pa = [pos[0] - length / 2, pos[1]];
   const pb = [pos[0] + length / 2, pos[1]];
@@ -83,9 +107,7 @@ function createFlipperShape({ pos, ra, rb, length, options }) {
     { x: pb[0], y: pb[1] - rb }
   ]);
 
-  const compound = M.Body.create({ parts: [circleA, circleB, poly] }, options);
-
-  return compound;
+  return M.Body.create({ parts: [circleA, circleB, poly] }, options);
 }
 
 function createFlipper({
@@ -155,6 +177,51 @@ function createRect({ engine, pos, dims, angle }) {
   M.World.add(engine.world, [rectangle]);
 }
 
+function createPlunger({ engine, pos, dims, angle }) {
+  const rectangle = M.Bodies.rectangle(pos[0], pos[1], dims[0], dims[1], {
+    angle: angle * DEG2RAD,
+    density: 0.3
+  });
+
+  const dx = 10;
+
+  const stiff = 0.01;
+  const aStiff = 0;
+  const damp = 0.001;
+  const len = 1;
+
+  const c1 = M.Constraint.create({
+    pointA: { x: -dx, y: 0 },
+    bodyA: rectangle,
+    pointB: { x: pos[0] - dx, y: pos[1] },
+    stiffness: stiff,
+    angularStiffness: aStiff,
+    damping: damp,
+    length: len
+  });
+
+  const c2 = M.Constraint.create({
+    pointA: { x: dx, y: 0 },
+    bodyA: rectangle,
+    pointB: { x: pos[0] + dx, y: pos[1] },
+    stiffness: stiff,
+    angularStiffness: aStiff,
+    damping: damp,
+    length: len
+  });
+
+  M.World.add(engine.world, [rectangle, c1, c2]);
+
+  beforeUpdateCbs.push(() => {
+    if (keyIsDown[KC_DOWN]) {
+      M.Body.applyForce(rectangle, rectangle.position, {
+        x: 0,
+        y: 10
+      });
+    }
+  });
+}
+
 function createSphere({ engine, pos, r }) {
   const sphere = M.Bodies.circle(pos[0], pos[1], r, {
     density: 0.002, // 0.001
@@ -164,6 +231,28 @@ function createSphere({ engine, pos, r }) {
 
   M.World.add(engine.world, [sphere]);
 }
+
+function createBumper({ engine, pos, r }) {
+  const sphere = M.Bodies.circle(pos[0], pos[1], r, {
+    //density: 0.0001, // 0.001
+    friction: 1,
+    restitution: 2
+  });
+
+  const constraint = M.Constraint.create({
+    bodyA: sphere,
+    pointA: { x: 0, y: 0 },
+    pointB: { x: pos[0], y: pos[1] },
+    //stiffness: 0.2,
+    //angularStiffness: 0,
+    //damping: 0.5,
+    length: 0
+  });
+
+  M.World.add(engine.world, [sphere, constraint]);
+}
+
+function createTriangleBumper({ engine, pos0, pos1, pos2, r }) {}
 
 function hookMouse({ engine, render }) {
   const mouse = M.Mouse.create(render.canvas);
@@ -201,7 +290,9 @@ function prepare() {
     beforeUpdateCbs.forEach(cb => cb());
   });
 
+  //setTimeout(function() {
   createSphere({ engine, pos: [W / 2 - 60, H / 2], r: 20 });
+  //}, 9000);
 
   createFlipper({
     engine,
@@ -253,8 +344,49 @@ function prepare() {
 
   createRect({
     engine,
+    pos: [W / 2 + 270, H * 0.51],
+    dims: [260, 12],
+    angle: 90
+  });
+
+  createRect({
+    engine,
     pos: [W / 2, H * 0.092],
     dims: [700, 24],
+    angle: 0
+  });
+
+  let lastSphereTime = new Date().valueOf();
+  beforeUpdateCbs.push(() => {
+    if (keyIsDown[KC_R]) {
+      const sphereTime = new Date().valueOf();
+      if (sphereTime - lastSphereTime < 500) {
+        return;
+      }
+      createSphere({ engine, pos: [W * 0.88, H * 0.5], r: 20 });
+      lastSphereTime = sphereTime;
+    }
+  });
+
+  createBumper({ engine, pos: [W * 0.7, H * 0.4], r: 48 });
+  createBumper({ engine, pos: [W * 0.6, H * 0.55], r: 32 });
+  createBumper({ engine, pos: [W * 0.55, H * 0.2], r: 48 });
+
+  const arc = createArc({
+    pos: [W * 0.805, H * 0.252],
+    r: 90,
+    a0: -90,
+    a1: 0,
+    dims: [12, 24],
+    steps: 12,
+    options: { isStatic: true }
+  });
+  M.World.add(engine.world, [arc]);
+
+  createPlunger({
+    engine,
+    pos: [W * 0.877, H * 0.6],
+    dims: [40, 32],
     angle: 0
   });
 
